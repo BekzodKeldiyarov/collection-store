b83ffab1fd559d72cebf74da24b8c2776ddab328
bekzodkeldiyarov <bek.bek.bek.bek72@gmail.com>
2022-06-13 02:23:26 +0500

    first commit

---
+++ src/main/java/com/bekzodkeldiyarov/collectionstore/CollectionStoreApplication.java
@@ -0,0 +1,13 @@
+package com.bekzodkeldiyarov.collectionstore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CollectionStoreApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CollectionStoreApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/bekzodkeldiyarov/collectionstore/CollectionStoreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bekzodkeldiyarov.collectionstore;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CollectionStoreApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
