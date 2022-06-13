893452f702f49b1cbe209db82919f3907c42ab4d
bekzodkeldiyarov <bek.bek.bek.bek72@gmail.com>
2022-06-13 17:43:32 +0500

    Added AdminController and Test file

---
+++ src/main/java/com/bekzodkeldiyarov/collectionstore/controllers/AdminController.java
@@ -0,0 +1,20 @@
+package com.bekzodkeldiyarov.collectionstore.controllers;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+@Slf4j
+public class AdminController {
+    @GetMapping("/admin")
+    public String getAdminPage() {
+        log.info("getAdminPage() function called");
+        return "admin/index";
+    }
+
+    @GetMapping("")
+    public String index() {
+        return "index";
+    }
+}
