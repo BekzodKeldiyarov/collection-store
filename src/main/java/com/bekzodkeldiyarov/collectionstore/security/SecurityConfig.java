63f5d98eb306c7953647a3d864a6384c3814b5e5
bekzodkeldiyarov <bek.bek.bek.bek72@gmail.com>
2022-06-13 16:00:19 +0500

    Spring Security Config added and PasswordConfig bean configured

---
+++ src/main/java/com/bekzodkeldiyarov/collectionstore/security/SecurityConfig.java
@@ -0,0 +1,39 @@
+package com.bekzodkeldiyarov.collectionstore.security;
+
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.password.NoOpPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+@EnableWebSecurity
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    private final PasswordEncoder passwordEncoder;
+
+    public SecurityConfig(PasswordEncoder passwordEncoder) {
+        this.passwordEncoder = passwordEncoder;
+    }
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http
+                .csrf().disable()
+                .authorizeRequests()
+                .antMatchers("/admin/**").hasRole("ADMIN_ROLE")
+                .antMatchers("**").permitAll()
+                .and()
+                .formLogin();
+    }
+
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth.inMemoryAuthentication()
+                .withUser("admin").password(passwordEncoder.encode("admin")).roles("ADMIN")
+                .and().withUser("user").password(passwordEncoder.encode("user")).roles("USER")
+                .and().withUser("reader").password(passwordEncoder.encode("reader")).roles("READER");
+    }
+
+}
